6f59e14a521c7da8337bfe7a66d7467e1386f4c5
Zevi Liao <jointjm@gmail.com>
2021-10-06 21:22:07 +0800

    updae audio v1

--- src/pages/media-player2.js
+++ src/pages/media-player2.js
@@ -1,38 +1,41 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import AudioSlider from './components/AudioSlider';
+import AudioFile from '../assets/hello.mp3';
 
 export default function App() {
-  const [sound, setSound] = React.useState();
 
-  async function playSound() {
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync(
-      require('../assets/hello.mp3')
-    );
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync();
-  }
-
-  async function stopSound() {
-    await sound.stopAsync();
-  }
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-        console.log('Unloading Sound');
-        sound.unloadAsync();
-      }
-      : undefined;
-  }, [sound]);
 
   return (
-    <View style={styles.container}>
-      <Button title="Play Sound" onPress={playSound} />
-      <Button title="Stop Sound" onPress={stopSound} />
+    <View style={[styles.StandardContainer, {
+      flex: 0,
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      marginTop: 100,
+      marginBottom: 5,
+    }]}>
+
+      <View style={{
+        flex: 0,
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}>
+
+        <Text style={[styles.StandardText, { flex: 5 }]}>
+          Audio name
+        </Text>
+
+        <TouchableOpacity
+          style={[{ flex: 1 }]}
+        >
+          <Text style={styles.StandardText}>Edit</Text>
+        </TouchableOpacity>
+
+
+      </View>
+
+      <AudioSlider audio={AudioFile} />
+
     </View>
   );
 }
