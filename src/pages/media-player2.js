63eda5eeb1001835c401cb6536f9f0892829ffc8
Zevi Liao <jointjm@gmail.com>
2021-10-08 09:00:07 +0800

    v3

--- src/pages/media-player2.js
+++ src/pages/media-player2.js
@@ -1,152 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import SeekBar from './components/SeekBar'
-import { Audio } from 'expo-av'
 
-export default class PlayerScreen extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      isPlaying: false,
-      playbackObject: null,
-      volume: 1.0,
-      isBuffering: false,
-      paused: true,
-      currentIndex: 0,
-      durationMillis: 1,
-      positionMillis: 0,
-      sliderValue: 0,
-      isSeeking: false,
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Player from './components/expo-audioplayer';
+
+
+// create a component
+const MyComponent = () => {
+  const TRACKS = [
+    {
+      title: 'Stressed Out',
+      subtitle: 'Twenty One Pilots',
+      albumArtUrl: "https://cdn-images-1.medium.com/max/1344/1*fF0VVD5cCRam10rYvDeTOw.jpeg",
+      audioUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+    },
+    {
+      title: 'Stressed Out',
+      subtitle: 'Twenty One Pilots',
+      albumArtUrl: "https://cdn-images-1.medium.com/max/1344/1*fF0VVD5cCRam10rYvDeTOw.jpeg",
+      audioUrl: "https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3"
     }
-  }
+  ];
+  return <Player tracks={TRACKS} displayArt={true}/>
+};
 
-  async componentDidMount() {
-    try {
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: false,
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-        playsInSilentModeIOS: true,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-        shouldDuckAndroid: true,
-        staysActiveInBackground: true,
-        playThroughEarpieceAndroid: true
-      })
-      this.loadAudio()
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  async loadAudio() {
-    const { currentIndex, isPlaying, volume } = this.state
-
-    try {
-      const playbackObject = new Audio.Sound()
-      const source = {
-        uri: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
-      }
-
-      const status = {
-        shouldPlay: isPlaying,
-        volume,
-      }
-
-      playbackObject.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
-      await playbackObject.loadAsync(source, status, true)
-      this.setState({ playbackObject })
-      var sliderValue = this.state.positionMillis / this.state.durationMillis
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  handlePlayPause = async () => {
-    const { isPlaying, playbackObject } = this.state
-    isPlaying ? await playbackObject.pauseAsync() : await playbackObject.playAsync()
-    this.setState({
-      isPlaying: !isPlaying
-    })
-  }
-
-  // _onSeekSliderValueChange = (value) => {
-  //   console.log("SEEK SLIDER VALUE", value);
-  //   if (this.playbackObject != null && !this.isSeeking) {
-  //     this.isSeeking = true;
-  //     this.shouldPlayAtEndOfSeek = this.state.shouldPlay;
-  //     this.playbackObject.pauseAsync();
-  //   }
-  // };
-
-  _onSeekSliderSlidingComplete = async (value) => {
-    const { isPlaying, playbackObject } = this.state
-    if (this.playbackObject != null) {
-      this.isSeeking = false;
-      const seekPosition = value * this.state.durationMillis;
-      alert(seekPosition)
-      if (this.shouldPlayAtEndOfSeek) {
-        this.playbackObject.playFromPositionAsync(seekPosition);
-      } else {
-        this.playbackObject.setPositionAsync(seekPosition);
-      }
-    }
-  };
-
-  onPlaybackStatusUpdate = status => {
-    this.setState({
-      isBuffering: status.isBuffering,
-      durationMillis: status.durationMillis,
-      positionMillis: status.positionMillis,
-    })
-  }
-
-  render() {
-    const { item } = {
-      item: {
-        title: 'a',
-        text: 'abc',
-      }
-    }
-    return (
-      <View style={globalStyles.container}>
-        <View style={globalStyles.subHeader}>
-          <Text style={globalStyles.title}>{item.title}</Text>
-        </View>
-        <View style={styles.text}>
-          <Text>{item.text}</Text>
-        </View>
-        <SeekBar
-          durationMillis={this.state.durationMillis}
-          positionMillis={this.state.positionMillis}
-          sliderValue={this.state.sliderValue}
-          onSlidingComplete={this._onSeekSliderSlidingComplete}
-        />
-        <TouchableOpacity
-          onPress={this.handlePlayPause}>
-          <Text style={styles.paragraph}>play</Text>
-        </TouchableOpacity>
-      </View>
-
-    )
-  }
-}
-
-const globalStyles = StyleSheet.create({
+// define your styles
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
   },
-  subHeader: {
-
-  },
-  title: {
-
-  }
 });
 
-const styles = StyleSheet.create({
-  text: {
-  }
-});
+//make this component available to the app
+export default MyComponent;
+
+
