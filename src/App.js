787f51016be4a032c6564c54c04d7aa9a862f548
Zevi Liao <jointjm@gmail.com>
2021-10-10 00:53:24 +0800

    createStackNavigator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Button, View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 // import MainScreen from './screens/MainScreen';
 import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
@@ -33,18 +33,21 @@ import Screen3 from './screens/Screen3';
 //   );
 // }
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Screen1"
         screenOptions={{
-          headerShown: false
-        }}>
+          headerShown: false,
+          animationEnabled: false
+        }}
+        >
         <Stack.Screen name="Screen1" component={Screen1} />
         <Stack.Screen name="Screen2" component={Screen2} />
-        <Stack.Screen name="Screen3" component={Screen3} />
+        <Stack.Screen name="Screen3" component={Screen3}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
