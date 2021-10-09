259384a9ddecbb2a56a72b3c9281b68b97c04851
Zevi Liao <jointjm@gmail.com>
2021-10-09 22:56:13 +0800

    drawer

--- src/App.js
+++ src/App.js
@@ -1,64 +1,7 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import MainScreen from './screens/MainScreen';
-import Screen1 from './screens/Screen1';
-import Screen2 from './screens/Screen2';
-import Screen3 from './screens/Screen3';
+import 'react-native-gesture-handler';
+import React from "react";
+import Root from "./views/Root";
 
-// function HomeScreen({ navigation }) {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <MainScreen></MainScreen>
-//     </SafeAreaView>
-//   );
-// }
-
-// function DetailsScreen({ navigation }) {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Details Screen</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() => navigation.push('Details')}
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-//       <Button title="Go back" onPress={() => navigation.goBack()} />
-//       <Button
-//         title="Go back to first screen in stack"
-//         onPress={() => navigation.popToTop()}
-//       />
-//     </View>
-//   );
-// }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen1"
-        screenOptions={{
-          headerShown: false
-        }}>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-        <Stack.Screen name="Screen3" component={Screen3} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default function App() {
+  return <Root />;
 }
-
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    height: '100%',
-    backgroundColor: '#2c3e50',
-  },
-});
-
-export default App;
