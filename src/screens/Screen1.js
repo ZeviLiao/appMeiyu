0ffaff55961119f2950205f651a03288f382871b
Zevi Liao <jointjm@gmail.com>
2021-10-09 15:51:36 +0800

    stack 3 nav

---
+++ src/screens/Screen1.js
@@ -0,0 +1,35 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+// create a component
+function Screen1({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Screen1</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Screen2')}
+      />
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      /> */}
+    </View>
+  );
+}
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#2c3e50',
+    },
+});
+
+//make this component available to the app
+export default Screen1;
