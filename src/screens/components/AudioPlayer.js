d6c1a32c481c91ef5e2b09757961085436d16774
Zevi Liao <jointjm@gmail.com>
2021-10-12 01:01:35 +0800

    add audio player

--- src/screens/components/AudioPlayer.js
+++ src/screens/components/AudioPlayer.js
@@ -1,46 +1,49 @@
-import * as React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { Video, AVPlaybackStatus } from 'expo-av';
 
-export default function AudioPlayer() {
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Player from './expo-audioplayer';
+
+
+// create a component
+const AudioPlayer = () => {
+  const TRACKS = [
+    {
+      title: 'Stressed Out',
+      subtitle: 'Twenty One Pilots',
+      albumArtUrl: "https://cdn-images-1.medium.com/max/1344/1*fF0VVD5cCRam10rYvDeTOw.jpeg",
+      audioUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+    },
+    {
+      title: 'Stressed Out',
+      subtitle: 'Twenty One Pilots',
+      albumArtUrl: "https://cdn-images-1.medium.com/max/1344/1*fF0VVD5cCRam10rYvDeTOw.jpeg",
+      audioUrl: "https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3"
+    }
+  ];
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      {/* <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
-        />
-      </View> */}
+    <View style={styles.audioPlayer}>
+      <Player tracks={TRACKS} displayArt={true} />
     </View>
-  );
-}
+  )
+};
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // backgroundColor: '#2c3e50',
-    },
-    video:{
-        width:656,
-        height:369
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+  audioPlayer: {
+    width: 338,
+    height: 369,
+  }
 });
+
+//make this component available to the app
+export default AudioPlayer;
+
 
