015631d6e9f66959cbb34a5a66a5dbc4b354825e
Zevi Liao <jointjm@gmail.com>
2021-10-11 11:51:32 +0800

    react-native-draggable-grid

--- src/screens/CardSortScreen.js
+++ src/screens/CardSortScreen.js
@@ -7,6 +7,7 @@ import HtmlViewer from './components/HtmlViewer';
 import HeaderTabs from './components/HeaderTabs';
 // import CardListItem from './components/CardListItem';
 import CardSortItem from './components/CardSortItem';
+import { DraggableGrid } from 'react-native-draggable-grid';
 
 // create a component
 const CourseListL1Screen = (props) => {
@@ -14,6 +15,104 @@ const CourseListL1Screen = (props) => {
     const [htmlData, setHtmlData] = useState('');
     // const { isMenu, isTabs } = props
     const { isMenu, isHeader, isTabs } = { isMenu: true, isHeader: false, isTabs: true }
+
+    let initialState = [
+        {
+            id: 1,
+            key: '1',
+            picUrl: '',
+            duration: '50:00',
+            title: '標題1',
+            subTitle: '副標1',
+            mediaType: 'video', // music
+            mediaSrc: ''
+        },
+        {
+            id: 2,
+            key: '2',
+            picUrl: '',
+            duration: '08:00',
+            title: '標題2',
+            subTitle: '副標2',
+            mediaType: 'music', // music
+            mediaSrc: ''
+        },
+        {
+            id: 3,
+            key: '3',
+            picUrl: '',
+            duration: '50:00',
+            title: '標題1',
+            subTitle: '副標1',
+            mediaType: 'video', // music
+            mediaSrc: ''
+        },
+        {
+            id: 4,
+            key: '4',
+            picUrl: '',
+            duration: '08:00',
+            title: '標題2',
+            subTitle: '副標2',
+            mediaType: 'music', // music
+            mediaSrc: ''
+        },
+        {
+            id: 5,
+            key: '5',
+            picUrl: '',
+            duration: '50:00',
+            title: '標題1',
+            subTitle: '副標1',
+            mediaType: 'video', // music
+            mediaSrc: ''
+        },
+        {
+            id: 6,
+            key: '6',
+            picUrl: '',
+            duration: '08:00',
+            title: '標題2',
+            subTitle: '副標2',
+            mediaType: 'music', // music
+            mediaSrc: ''
+        },
+        {
+            id: 7,
+            key: '7',
+            picUrl: '',
+            duration: '50:00',
+            title: '標題1',
+            subTitle: '副標1',
+            mediaType: 'video', // music
+            mediaSrc: ''
+        },
+        {
+            id: 8,
+            key: '8',
+            picUrl: '',
+            duration: '08:00',
+            title: '標題2',
+            subTitle: '副標2',
+            mediaType: 'music', // music
+            mediaSrc: ''
+        },
+    ]
+
+    let initialState1 = [
+        { name: '1', key: 'one' },
+        { name: '2', key: 'two' },
+        { name: '3', key: 'three' },
+        { name: '4', key: 'four' },
+        { name: '5', key: 'five' },
+        { name: '6', key: 'six' },
+        { name: '7', key: 'seven' },
+        { name: '8', key: 'eight' },
+        { name: '9', key: 'night' },
+        { name: '0', key: 'zero' },
+    ]
+
+    const [data, setData] = useState(initialState)
 
     const showDocById = (id) => {
         setHtmlData('data' + id)
@@ -30,80 +129,7 @@ const CourseListL1Screen = (props) => {
         },
     ]
 
-    const mediaList = [
-        {
-            id: 1,
-            picUrl: '',
-            duration: '50:00',
-            title: '標題1',
-            subTitle: '副標1',
-            mediaType: 'video', // music
-            mediaSrc: ''
-        },
-        {
-            id: 2,
-            picUrl: '',
-            duration: '08:00',
-            title: '標題2',
-            subTitle: '副標2',
-            mediaType: 'music', // music
-            mediaSrc: ''
-        },
-        {
-            id: 3,
-            picUrl: '',
-            duration: '50:00',
-            title: '標題1',
-            subTitle: '副標1',
-            mediaType: 'video', // music
-            mediaSrc: ''
-        },
-        {
-            id: 4,
-            picUrl: '',
-            duration: '08:00',
-            title: '標題2',
-            subTitle: '副標2',
-            mediaType: 'music', // music
-            mediaSrc: ''
-        },
-        {
-            id: 5,
-            picUrl: '',
-            duration: '50:00',
-            title: '標題1',
-            subTitle: '副標1',
-            mediaType: 'video', // music
-            mediaSrc: ''
-        },
-        {
-            id: 6,
-            picUrl: '',
-            duration: '08:00',
-            title: '標題2',
-            subTitle: '副標2',
-            mediaType: 'music', // music
-            mediaSrc: ''
-        },
-        {
-            id: 7,
-            picUrl: '',
-            duration: '50:00',
-            title: '標題1',
-            subTitle: '副標1',
-            mediaType: 'video', // music
-            mediaSrc: ''
-        },
-        {
-            id: 8,
-            picUrl: '',
-            duration: '08:00',
-            title: '標題2',
-            subTitle: '副標2',
-            mediaType: 'music', // music
-            mediaSrc: ''
-        },
-    ]
+
 
     const cardItemOpts = { // card 1
         size: {
@@ -131,6 +157,21 @@ const CourseListL1Screen = (props) => {
     //     }
     // }
 
+    const render_item = (item) => {
+        return (
+            // <View
+            //     style={styles.item}
+            //     key={item.key}
+            // >
+            //     <Text style={styles.item_text}>{item.name}</Text>
+            // </View>
+            <View key={item.key}
+                style={[styles.listItem]}>
+                <CardSortItem opts={cardItemOpts} course={item} />
+            </View>
+        );
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             {isMenu && <LeftMenu style={styles.leftMenu}></LeftMenu>}
@@ -146,7 +187,7 @@ const CourseListL1Screen = (props) => {
                     tabClick={(docId) => showDocById(docId)} />}
                 <View>
                     <Text style={styles.sort}>sort</Text>
-                    <ScrollView style={styles.listScrollWrapper}>
+                    {/* <ScrollView style={styles.listScrollWrapper}>
                         <View style={styles.listWrapper}>
                             {
                                 mediaList.map(m => {
@@ -158,6 +199,18 @@ const CourseListL1Screen = (props) => {
                                     )
                                 })
                             }
+                        </View>
+                    </ScrollView> */}
+                    <ScrollView style={styles.listScrollWrapper}>
+                        <View style={styles.listWrapper}>
+                            <DraggableGrid
+                                numColumns={3}
+                                renderItem={render_item}
+                                data={data}
+                                onDragRelease={(data) => {
+                                    setData(data);// need reset the props data sort after drag release
+                                }}
+                            />
                         </View>
                     </ScrollView>
                 </View>
@@ -209,6 +262,11 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 20,
         right: 20
+    },
+    item: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'gray'
     }
 });
 
