59eec39443b4fc9d9d3e75d21dabbf12dd1bcf6a
Zevi Liao <jointjm@gmail.com>
2021-10-15 00:04:10 +0800

    add book popup

--- src/screens/allCourse/CourseMediaListL3Screen.js
+++ src/screens/allCourse/CourseMediaListL3Screen.js
@@ -1,15 +1,19 @@
 //import liraries
 import React, { Component, useState } from 'react';
-import { ScrollView, SafeAreaView, Text, StyleSheet, View, Button } from 'react-native';
+import { ScrollView, SafeAreaView, Text, StyleSheet, View, Button, Modal, Pressable } from 'react-native';
 import LeftMenu from '../components/LeftMenu';
 import ScreenHeader from '../components/ScreenHeader';
 import HtmlViewer from '../components/HtmlViewer';
 import TrainHeaderTabs from '../components/TrainHeaderTabs';
 // import CardListItem from '../components/CardListItem';
 import TrainMediaCardL3Item from '../components/TrainMediaCardL3Item';
+import SvgUri from "expo-svg-uri";
+
 
 // create a component
 const CourseMediaListL3Screen = ({ navigation, route }) => {
+
+    const [modalVisible, setModalVisible] = useState(false);
 
     const { courseL2 } = route.params;
 
@@ -131,7 +135,7 @@ const CourseMediaListL3Screen = ({ navigation, route }) => {
         },
         fontTitle: {
             fontSize: 20,
-            fontWeight:'bold',
+            fontWeight: 'bold',
             // color: 'rgb(26,26,26)',
             // textAlign: 'center',
         },
@@ -148,6 +152,22 @@ const CourseMediaListL3Screen = ({ navigation, route }) => {
 
     return (
         <SafeAreaView style={styles.container}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                    <SvgUri source={require('../../assets/icon/icon_book-pop.svg')} />
+                        <Text style={styles.modalText}>已加入我的備課</Text>
+                    </View>
+                </View>
+            </Modal>
             {isMenu && <LeftMenu style={styles.leftMenu}></LeftMenu>}
             <View style={styles.mainContent}>
                 {isHeader &&
@@ -168,6 +188,12 @@ const CourseMediaListL3Screen = ({ navigation, route }) => {
                                         style={[styles.listItem, (cardItemOpts.size.width < 200) ? { width: '25%' } : {}]}>
                                         <TrainMediaCardL3Item opts={cardItemOpts} course={m}
                                             onPress={() => navTo({ courseL3: m })}
+                                            onAddBook={() => {
+                                                setModalVisible(true)
+                                                setTimeout(() => {
+                                                    setModalVisible(false)
+                                                }, 1300)
+                                            }}
                                         />
                                     </View>
                                 )
@@ -219,6 +245,51 @@ const styles = StyleSheet.create({
         // justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 30,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        fontSize:28,
+        marginTop:30,
+        marginHorizontal:80,
+        marginBottom: 15,
+        textAlign: "center",
+        color:'rgb(26,26,26)'
     }
 });
 
--- src/screens/components/TrainMediaCardL3Item.js
+++ src/screens/components/TrainMediaCardL3Item.js
@@ -1,10 +1,11 @@
 //import liraries
 import React, { Component } from 'react';
 import { TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native';
+import SvgUri from "expo-svg-uri";
 
 // create a component
 const TrainMediaCardL2Item = (props) => {
-    let { opts, course, onPress } = props
+    let { opts, course, onPress, onAddBook} = props
     return (
         <TouchableOpacity onPress={onPress}>
             <View style={[styles.container, opts.size]}>
@@ -17,6 +18,11 @@ const TrainMediaCardL2Item = (props) => {
                 <View style={{ padding: 15 }} >
                     <Text style={[{ marginBottom: 10 }, opts.fontTitle]}>{course.title}</Text>
                     <Text style={opts.fontDesc}>{course.subTitle} </Text>
+                </View>
+                <View style={styles.addBook}>
+                    <TouchableOpacity onPress={() => onAddBook()}>
+                        <SvgUri source={require('../../assets/icon/icon_book.svg')} />
+                    </TouchableOpacity>
                 </View>
 
             </View>
@@ -59,6 +65,15 @@ const styles = StyleSheet.create({
     // desc: {
     //     fontSize: 24
     // }
+    addBook: {
+        // width: 30,
+        // height: 30,
+        position: 'absolute',
+        // backgroundColor: '#00ffaa',
+        right: 15,
+        bottom: 15,
+        // borderRadius: 15
+    },
 });
 
 //make this component available to the app
